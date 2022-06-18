a7208b6c3a764f92deefa5a0674d62097339a561
rahulrauki <rahulrauki@gmail.com>
2022-06-18 21:39:30 +0530

    one liner

--- binary-search-704.py
+++ binary-search-704.py
@@ -1,4 +1,4 @@
-class Solution: # O(log n) time complexity 
+class Solution: # O(log n) time complexity beats 87,77
     def search(self, nums: List[int], target: int) -> int:
         samp = nums
         while len(samp) > 0:
@@ -7,3 +7,7 @@ class Solution: # O(log n) time complexity
             elif target < mid: samp = samp[0:len(samp)//2]
             else: samp = samp[1+len(samp)//2:]
         return -1
+
+class Solution1: # One liner beats 99.24 97.28
+    def search(self, nums: List[int], target: int) -> int:
+        return nums.index(target) if target in nums else -1
