c4603347933f22b7ef3ac7cdebfd7864420ce28d
rahulrauki <rahulrauki@gmail.com>
2022-06-18 21:57:59 +0530

    typical left right method

--- binary-search-704.py
+++ binary-search-704.py
@@ -1,4 +1,4 @@
-class Solution: # O(log n) time complexity beats 87,77
+class Solution: # O(log n) time complexity beats 87,77 Number 2
     def search(self, nums: List[int], target: int) -> int:
         samp = nums
         while len(samp) > 0:
@@ -8,6 +8,16 @@ class Solution: # O(log n) time complexity beats 87,77
             else: samp = samp[1+len(samp)//2:]
         return -1
 
-class Solution1: # One liner beats 99.24 97.28
+class Solution1: # One liner beats 99.24 97.28 Number 1
     def search(self, nums: List[int], target: int) -> int:
         return nums.index(target) if target in nums else -1
+
+class Solution2: #Number 3
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r ) // 2
+            if nums[mid] < target : l = mid + 1
+            elif nums[mid] > target : r = mid - 1
+            else: return mid
+        return -1
