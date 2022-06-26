26a561c53792ba13596459a012315cd360d1e389
rahulrauki <rahulrauki@gmail.com>
2022-06-26 19:51:12 +0530

    sliding window - index subtraction

--- maximum-points-1423.py
+++ maximum-points-1423.py
@@ -9,3 +9,15 @@ class Solution:#
             window = sum(cardPoints[:k-i]) + sum(cardPoints[-i:])
             if window > maxi: maxi = window
         return maxi
+
+#The Solution by subtracting the index from the inital window sum - method
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        leftSum,rightSum = sum(cardPoints[:k]),0
+        maxi = leftSum
+        for i in range(k):
+            leftSum -= cardPoints[k-i-1]# [k-(i+1)] or just [k-i] if i starts from 1
+            rightSum += cardPoints[-i-1]
+            add = leftSum + rightSum
+            if add > maxi: maxi = add
+        return maxi
